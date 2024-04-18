f9c9524fc5b645cdcedf96ccb4526b6e033ee95d
macimas <macimas@vivaldi.net>
2024-04-18 23:08:17 +0800

    @@

--- @/index.js
+++ @/index.js
@@ -3,6 +3,18 @@ const query = params.get("q");
 const separator = params.get("s");
 const urls = params.get("urls")?.split(separator);
 
+function boop(text) {
+	alert(text);
+	throw text;
+}
+
+if (!query) boop("no query?");
+if (!seperator) boop("no seperator?");
+if (!urls) boop("no urls?");
+
+display_query.innerText = query;
+display_amount.innerText = urls.length;
+
 urls.forEach(url => {
 	console.log(url);
 
